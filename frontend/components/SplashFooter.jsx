cb23d634551236ca2842dc34cd42f04708e001f2
hisrael1 <harrisonisrael23@gmail.com>
2022-08-04 01:03:04 -0400

    splash footer

---
+++ frontend/components/SplashFooter.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const SplashFooter = () => {
+
+    return (
+        <div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <p id="inspiration">
+                Inspiration for your next trip
+            </p>
+
+            <br />
+            <br />
+            <br />
+
+            <div id="city-container" class="cities">
+                <div id='montauk' class="city">
+                    <img src={window.montauk} id="montauk"/>
+                    <p class="cty-text">Montauk</p>
+                </div>
+                <div id='lakegeorge' class="city">
+                    <img src={window.lakegeorge} id="lakegeorge"/>
+                    <p class="cty-text">Lake George</p>
+                </div>
+                <div id='newport' class="city">
+                    <img src={window.newport} id="newport"/>
+                    <p class="cty-text">Newport</p>
+                </div>
+                <div id='scranton' class="city">
+                    <img src={window.scranton} id="scranton"/>
+                    <p class="cty-text">
+                        Scranton
+                    </p>
+                </div>
+            </div>
+
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
+
+            <div id="footer">
+                <div>
+                    <p class="bold-footer">
+                        About
+                    </p>
+                </div>
+
+                <div>
+                    <p class="bold-footer">
+                        Resources
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SplashFooter;
