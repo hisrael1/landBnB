3ee2b92ad0f3af01613ed92e7bc888a8e92dfeba
hisrael1 <harrisonisrael23@gmail.com>
2022-08-08 18:59:34 -0400

    dropdown

--- frontend/components/Menu.jsx
+++ frontend/components/Menu.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom';
+import Dropdown from './Dropdown';
 
-const Menu = () => {
+{/* My next step is to fully replace the css dropdown with the onClick dropdown
+is null correct in my conditional rendering???
+Then construct modals for sign in, sign up, sign out
+then listings component */}
 
-    return (
-        <div class="menu-container">
-            <img src={window.hamburger_icon} id="hamburger-icon"/>
-                <p class="menu-dropdown"> 
-                    Menu Options 
-                    <br />
-                    <li><Link to={'/login'}>Login</Link></li>
-                    <br />
-                    <li><Link to={'/signup'}>Sign Up</Link></li>
-                    <br />
-                    <li><Link to={'/logout'}>Logout</Link></li>
-                </p>    
-        </div>
-    )
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { renderDropdown: false }
+    }
+
+    render() {
+        return (
+            <div class="menu-container" onClick={ () => {this.setState({ renderDropdown: !this.state.renderDropdown }) } }>
+                <img src={window.hamburger_icon} id="hamburger-icon"/>
+                    {this.state.renderDropdown ?
+                        <Dropdown />
+                        :
+                        null
+                    }
+            </div>
+        )
+    }
 }
 
 export default Menu;
