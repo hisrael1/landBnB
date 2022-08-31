2c8074814a6d0c2b2972c2b2d9e13a0a06f84faf
hisrael1 <harrisonisrael23@gmail.com>
2022-08-31 16:30:23 -0400

    conditional session rendering

--- frontend/components/dropdown/Dropdown.jsx
+++ frontend/components/dropdown/Dropdown.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 
-const Dropdown = ({openModal, logout}) => {
+const Dropdown = ({session, openModal, logout}) => {
 
-    return (
-        <main className="menu-dropdown"> 
-            <br />
-            <main onClick={() => openModal("login")}>Login</main>
-            <br />
-            <br />
-            <main onClick={() => openModal("signup")}>Sign Up</main>
-            <br />
-            <br />
-            <main onClick={() => logout()}>Logout</main>
-            <br />
-        </main>    
-    )
+    if (!session.id) {
+        return (
+            <div className="menu-dropdown"> 
+                <div className="session-dropdown-option" onClick={() => openModal("login")}>Login</div>
+                <div className="session-dropdown-option" onClick={() => openModal("signup")}>Sign Up</div>
+            </div>    
+        )
+    } else {
+        return (
+            <div className="menu-dropdown"> 
+                <div className="session-dropdown-option" onClick={() => logout()}>Logout</div>
+            </div>    
+        )
+    }
 }
 
 export default Dropdown;
--- frontend/components/dropdown/DropdownContainer.js
+++ frontend/components/dropdown/DropdownContainer.js
@@ -3,10 +3,14 @@ import Dropdown from "./Dropdown";
 import { connect } from 'react-redux';
 import { logout } from "../../actions/session_actions";
 
+const mapStateToProps = (state) => ({
+    session: state.session
+})
+
 const mapDispatchToProps = (modal) => ({
     openModal: (modal) => dispatch(openModal(modal)),
     logout: () => dispatch(logout())
 })
 
-const DropdownContainer = connect(null, mapDispatchToProps)(Dropdown);
+const DropdownContainer = connect(mapStateToProps, mapDispatchToProps)(Dropdown);
 export default DropdownContainer;
