557129fcc7f9ab326255f8f46fa23b489140c470
hisrael1 <harrisonisrael23@gmail.com>
2022-09-05 20:49:05 -0400

    listings css static

--- frontend/components/Listings.jsx
+++ frontend/components/Listings.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import Header from "./Header.jsx"
 
 const Listings = props => {
 
     return (
-        <ul>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-            <h1> Listings Component </h1>
-        </ul>
+        <div>
+
+            <Header />
+            <div className="listings-and-map-container">
+                <div className="listings-outer-container">
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                    <p> Listings Component </p>
+                </div>
+                <div className="map-api">
+                    Map Api Placeholder
+                </div>
+            </div>
+        </div>
     )
 
 };
