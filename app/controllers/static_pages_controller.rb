a8bc71195f4503facb933f01a7894bede188b498
hisrael1 <harrisonisrael23@gmail.com>
2022-11-21 20:43:48 -0500

    heroku deployment

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,3 @@
-# Did I create the proj with --database-postgresql + the other command?
-# Is my controller named correctly?
 class StaticPagesController < ApplicationController
     def root
     end
