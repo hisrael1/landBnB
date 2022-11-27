4015f39480f364f505d36bbd5c4c0b90286db3c7
hisrael1 <harrisonisrael23@gmail.com>
2022-11-27 18:14:01 -0500

    no aws

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,12 +4,12 @@ import SignupFormContainer from "./session/SignupFormContainer";
 import { Redirect, Switch, Route } from "react-router-dom";
 import { AuthRoute } from "../util/route_util";
 import Splash from "./splash/Splash";
-import ListingsContainer from "./listings/ListingsContainer";
-import UserContainer from "./user/UserContainer";
-import NewListingContainer from "./listings/NewListingContainer";
-import ShowListingContainer from "./listings/ShowListingContainer";
-import CreateReviewContainer from "./reviews/CreateReviewContainer";
-import Trips from "./booking/Trips";
+// import ListingsContainer from "./listings/ListingsContainer";
+// import UserContainer from "./user/UserContainer";
+// import NewListingContainer from "./listings/NewListingContainer";
+// import ShowListingContainer from "./listings/ShowListingContainer";
+// import CreateReviewContainer from "./reviews/CreateReviewContainer";
+// import Trips from "./booking/Trips";
 
 
 const App = () => (
@@ -17,12 +17,12 @@ const App = () => (
         <AuthRoute path="/login" component={LoginFormContainer} />
         <AuthRoute path="/signup" component={SignupFormContainer} />
         <Route exact path = "/" component={Splash} />
-        <Route path = "/listings/:query" component={ListingsContainer} />
-        <Route path = "/users/show/" component={UserContainer}/>
+        {/* <Route path = "/listings/:query" component={ListingsContainer} /> */}
+        {/* <Route path = "/users/show/" component={UserContainer}/>
         <Route path = "/listing/new/" exact component={NewListingContainer}/>
         <Route path = "/listing/:id/review/new/" component={CreateReviewContainer}/> 
         <Route path = "/listing/:id/" component={ShowListingContainer}/>
-        <Route path = "/user/:id/bookings" component={Trips}/>
+        <Route path = "/user/:id/bookings" component={Trips}/> */}
                
     </Switch>
 );
