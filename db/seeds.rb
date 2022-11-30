204844c33467ddf440602c3ab1e21686a4bd80a6
hisrael1 <harrisonisrael23@gmail.com>
2022-11-30 12:52:36 -0500

    first listing with photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User.destroy_all
-User.create(first_name: "Demo", last_name: "User", email: "user@demo.com", password: "password")
+# User.create(first_name: "Demo", last_name: "User", email: "user@demo.com", password: "password")
 
 # Listing.destroy_all
-Listing.create({
+listing1 = Listing.create({
         title: "Luxury Miami Villa",
         description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -29,3 +29,9 @@ Listing.create({
         lat: 12.32,
         lng: 132.74
     })
+
+listing1.photos.attach(io: File.open('app/assets/images/miami-villa-1.jpg'), filename: 'cool_house.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/miami-villa-2.webp'), filename: 'miami-villa-2.webp')
+listing1.photos.attach(io: File.open('app/assets/images/miami-villa-3.jpg'), filename: 'miami-villa-3.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/miami-villa-4.jpg'), filename: 'miami-villa-4.jpg')
+listing1.photos.attach(io: File.open('app/assets/images/miami-villa-5.jpg'), filename: 'miami-villa-5.jpg')
