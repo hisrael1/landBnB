93944ac2947540e553659e7e63211934479a6902
hisrael1 <harrisonisrael23@gmail.com>
2022-12-09 00:16:18 -0500

    no listings redirect

--- frontend/components/user/UserInfo.jsx
+++ frontend/components/user/UserInfo.jsx
@@ -8,12 +8,10 @@ const UserInfo = (props) => {
             {user ? <p id="user-info-hi"> Hi, I'm {user.first_name} {user.last_name} </p> : null}
             <p id="user-info-about"> About </p>
             <div className="user-info-border"></div>
-            <p id="user-listings-header">&#9830; 2 Listings</p>
+            <p id="user-listings-header">&#9830; 5 Listings</p>
             <button id="user-add-listing" onClick={() => history.push('/listing/new')}> Add Listing </button>
-            {/* Clicking this should render all the users listings */}
             <div className="user-info-border"></div>
             <p id="user-listings-header">&#9733; 0 Reviews </p>
-            {/* Clicking this should render all the users reviews */}
         </div>
     )
 }
