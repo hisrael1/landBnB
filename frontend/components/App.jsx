65c754ff5d3a8102e72eaf59df9fe525314877bc
hisrael1 <harrisonisrael23@gmail.com>
2022-12-13 00:28:16 -0500

    mobile zoom out

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,14 +9,11 @@ import UserContainer from "./user/UserContainer";
 import NewListingContainer from "./listings/NewListingContainer";
 import ShowListingContainer from "./listings/ShowListingContainer";
 import CreateReviewContainer from "./reviews/CreateReviewContainer";
-// import Trips from "./booking/Trips";
 import TripsContainer from "./booking/TripsContainer";
 
 
 const App = () => (
     <Switch>
-        {/* <AuthRoute path="/login" component={LoginFormContainer} />
-        <AuthRoute path="/signup" component={SignupFormContainer} /> */}
         <Route exact path = "/" component={Splash} />
         <Route path = "/listings/:query" component={ListingsContainer} />
         <Route path = "/users/show/" component={UserContainer}/>
